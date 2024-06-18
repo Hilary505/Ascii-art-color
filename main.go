73c7e6df670ac5da0cc83759085b6244c6ee7d17
Hilary Oduor <145471459+Hilary505@users.noreply.github.com>
2024-06-18 06:14:45 +0300

    Add files via upload
    feat : modifed the code

--- main.go
+++ main.go
@@ -4,19 +4,21 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
-	a "ascii/ascii_art"
+	"ascii/ascii_art"
 )
 
 func main() {
 	// Default arguments
 	input := ""
-	bannerFile := "standard.txt"
+	bannerFile := "shadow.txt"
 	subString := " "
 
-	flgColor := flag.String("color", "reset", "Color")
+	var flgColor = flag.String("color", "reset", "Color")
+	var flagOutput = flag.String("output", "", "my bannerFiele")
 	flag.Parse()
-	color := a.ColorPicker(*flgColor)
+	color := ascii_art.ColorPicker(*flgColor)
 	// println("Color: ", *flgColor)
 
 	args := flag.Args()
@@ -40,15 +42,41 @@ func main() {
 			bannerFile = args[2]
 		}
 	default:
-		fmt.Println(`Usage: go run . --color=<color> <substring to be colored> "string to be printed" <banner file>`)
-		os.Exit(1)
+		fmt.Println("Usage: go run . [STRING] [BANNER]")
+		fmt.Println()
+
+		fmt.Println("EX: go run . something standard")
+		os.Exit(0)
+	}
+	if !strings.HasSuffix(bannerFile, ".txt") {
+		bannerFile += ".txt"
 	}
 
-	contents, err := a.GetFile(bannerFile)
+	contents, err := ascii_art.GetFile(bannerFile)
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
 
-	fmt.Print(a.ProcessInput(contents, input, color, subString))
+	output := ascii_art.ProcessInput(contents, input, color, subString)
+
+	if *flagOutput != "" {
+		err = os.WriteFile(*flagOutput, []byte(output), 0644)
+		if err != nil {
+			fmt.Println("An erreor just occured while writing File:")
+			os.Exit(1)
+		}
+	}else{
+		fmt.Print(output)
+	}
+
 }
+
+// var fame = flag.String("output", "banner.txt", "my file input")
+// 	flag.Parse()
+// 	args1 := flag.Args()
+// 	text := args1[0] + ("\n")
+// 	err = os.WriteFile(*fame, []byte(text), 0644)
+// 	if err != nil {
+// 		return
+// 	}
